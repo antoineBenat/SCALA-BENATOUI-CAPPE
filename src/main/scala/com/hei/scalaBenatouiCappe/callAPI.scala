e8cacc55b65f54d97dc076df1a984e5e0c3a7e4c
Thomas.Cappe <Azerty62280>
2021-10-20 17:59:33 +0200

    Enregistrement

--- src/main/scala/com/hei/scalaBenatouiCappe/callAPI.scala
+++ src/main/scala/com/hei/scalaBenatouiCappe/callAPI.scala
@@ -11,21 +11,21 @@ object Main {
     val json : JsValue  = Json.parse(request.body)
     val now = Calendar.getInstance()
 
-    val allElement = Array.ofDim[String](500,1)
+    val allElement = Array.ofDim[Any](500,1)
     var index =0;
     //val gle : Array[String] =new Array[String](request.body.size);
     for( x <-0 to 500) {
       now.set(2021,10,12)
-      val element: Array[String] = new Array[String](6);
+      val element: Array[Any] = new Array[Any](6);
       now.set(Calendar.DATE, (now.get(Calendar.DATE) - x))
       try {
         val date: String = now.get(Calendar.YEAR) + "-" +  "%02d".format(now.get(Calendar.MONTH)) + "-" + "%02d".format(now.get(Calendar.DATE))
         element(0) = '"'+date+'"'
-        element(1) = (json("Time Series (Daily)")(date)("1. open").toString())
-        element(2) = (json("Time Series (Daily)")(date)("2. high").toString())
-        element(3) = (json("Time Series (Daily)")(date)("3. low").toString())
-        element(4) = (json("Time Series (Daily)")(date)("4. close").toString())
-        element(5) = (json("Time Series (Daily)")(date)("5. volume").toString())
+        element(1) = (json("Time Series (Daily)")(date)("1. open").toString()).slice(1,(json("Time Series (Daily)")(date)("1. open").toString()).size-2).toDouble
+        element(2) = (json("Time Series (Daily)")(date)("2. high").toString()).slice(1,(json("Time Series (Daily)")(date)("2. high").toString()).size-2).toDouble
+        element(3) = (json("Time Series (Daily)")(date)("3. low").toString()).slice(1,(json("Time Series (Daily)")(date)("3. low").toString()).size-2).toDouble
+        element(4) = (json("Time Series (Daily)")(date)("4. close").toString()).slice(1,(json("Time Series (Daily)")(date)("4. close").toString()).size-2).toDouble
+        element(5) = (json("Time Series (Daily)")(date)("5. volume").toString()).slice(1,(json("Time Series (Daily)")(date)("5. volume").toString()).size-2).toDouble
         allElement(index) = element
         index=index+1
       }catch{
@@ -34,13 +34,23 @@ object Main {
         }
       }
     }
-
+/*
     //Affichage de la liste compléte
    var x=0
     while (allElement(x).mkString(",") != "null") {
       println(allElement(x).mkString(","))
       x+=1
+    }*/
+    val i = allElement(3)(4).asInstanceOf[Double].toInt
+    println(i+1)
+    println(i)
+
+    def toInt(anyVal: Any): Int ={
+      val i = anyVal.asInstanceOf[Double].toInt
+      return i
     }
+
+
   }
 }
 
