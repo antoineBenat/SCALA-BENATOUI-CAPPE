5b8cab78d63a3697f01db048864c66e3b3468942
Thomas.Cappe <Azerty62280>
2021-10-26 15:43:59 +0200

    Correction de l'api

--- src/main/scala/com/hei/scalaBenatouiCappe/callAPI.scala
+++ src/main/scala/com/hei/scalaBenatouiCappe/callAPI.scala
@@ -1,56 +1,54 @@
 package com.hei.scalaBenatouiCappe
 import play.api.libs.json.{JsValue, Json}
 import scalaj.http.{Http, HttpResponse}
-
+import scala.collection.mutable.ArrayBuffer
 import java.util.Calendar
 
-
-object Main {
-  def main(args: Array[String]): Unit = {
+object CallAPI{
+  def callAPI():ArrayBuffer[Array[Any]]={
     val request: HttpResponse[String] = Http("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DAI.DEX&outputsize=full&datatype=json&apikey=EXALLWIOROD5BP80").param("q","monkeys").asString
     val json : JsValue  = Json.parse(request.body)
     val now = Calendar.getInstance()
     val names: List[String] = List("1. open","2. high","3. low","4. close","5. volume");
-    val allElement = Array.ofDim[Any](500,1)
-    var index =0;
+    val allElement = ArrayBuffer[Array[Any]]()
     for( x <-0 to 500) {
-      now.set(2021,10,12)
+      now.set(2021,10,26)
       val element: Array[Any] = new Array[Any](6);
       now.set(Calendar.DATE, (now.get(Calendar.DATE) - x))
       try {
         val date: String = now.get(Calendar.YEAR) + "-" +  "%02d".format(now.get(Calendar.MONTH)) + "-" + "%02d".format(now.get(Calendar.DATE))
-        element(0) = '"'+date+'"'
-        element(1) = (json("Time Series (Daily)")(date)("1. open").toString()).slice(1,(json("Time Series (Daily)")(date)("1. open").toString()).size-2).toDouble
-        element(2) = (json("Time Series (Daily)")(date)("2. high").toString()).slice(1,(json("Time Series (Daily)")(date)("2. high").toString()).size-2).toDouble
-        element(3) = (json("Time Series (Daily)")(date)("3. low").toString()).slice(1,(json("Time Series (Daily)")(date)("3. low").toString()).size-2).toDouble
-        element(4) = (json("Time Series (Daily)")(date)("4. close").toString()).slice(1,(json("Time Series (Daily)")(date)("4. close").toString()).size-2).toDouble
-        element(5) = (json("Time Series (Daily)")(date)("5. volume").toString()).slice(1,(json("Time Series (Daily)")(date)("5. volume").toString()).size-2).toDouble
-        allElement(index) = element
-        index=index+1
+        element(0) = date
+        for (x <- 1 to 5){
+          element(x) = (json("Time Series (Daily)")(date)(names(x-1)).toString()).slice(1,(json("Time Series (Daily)")(date)(names(x-1)).toString()).size-2).toDouble
+        }
+        allElement += element
       }catch{
         case exception: Exception=>{
         }
       }
     }
+  return allElement
+  }
+}
+object Main {
 
-    //Affichage de la liste compléte
-   var x=0
-    while (allElement(x).mkString(",") != "null") {
-      println(allElement(x).mkString(","))
-      x+=1
+  //Affichage de la liste compléte
+  def printTab(allElement:ArrayBuffer[Array[Any]]) {
+    for (element <- allElement) {
+      println(element.mkString(" ", " ", " "))
     }
-
-    //Fonction permettant de récupérer un Int depuis un "Any"
-    def toInt(any: Any): Int ={
-      val i = any.asInstanceOf[Double].toInt
-      return i
-    }
-    //Exemple de valeur pour le 4éme élément de la 3éme jourée
-    val j=toInt(allElement(3)(4))
+  }
+  //Fonction permettant de récupérer un Int depuis un "Any"
+  def toDouble(any: Any): Double ={
+    val i = any.asInstanceOf[Double]
+    return i
+  }
 
 
-
-
+  def main(args: Array[String]): Unit = {
+    val tab =  CallAPI.callAPI()
+    print(toDouble(tab(1)(1)))
+    printTab(tab)
   }
 }
 
