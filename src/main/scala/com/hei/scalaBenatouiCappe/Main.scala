a77bbe81332e0e653a2144f27e778723ec6d397d
Thomas.Cappe <Azerty62280>
2021-10-26 16:01:34 +0200

    Organisation du projet et amélioration

--- src/main/scala/com/hei/scalaBenatouiCappe/Main.scala
+++ src/main/scala/com/hei/scalaBenatouiCappe/Main.scala
@@ -1,21 +1,16 @@
 package com.hei.scalaBenatouiCappe
+
 import com.hei.scalaBenatouiCappe.Fonctions.FonctionsAPI
-import play.api.libs.json.{JsValue, Json}
-import scalaj.http.{Http, HttpResponse}
-
-import scala.collection.mutable.ArrayBuffer
-import java.util.Calendar
 import com.hei.scalaBenatouiCappe.Objets.CallAPI
-
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     val callAPI: CallAPI= new CallAPI()
     val fonctionsAPI: Fonctions.FonctionsAPI= new FonctionsAPI()
     val tab =  callAPI.callAPI()
+
     print(fonctionsAPI.toDouble(tab(1)(1)))
     fonctionsAPI.printTab(tab)
   }
 }
-
-//Pour afficher un élément => allElement(..)(..)
